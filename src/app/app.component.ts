73b6601b5657e8c9b4cccf8b5ac028a57624111a
Jonathan Gamble <jdgamble555@gmail.com>
2024-03-02 09:21:47 -0600

    load server data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-cloudflare';
+  data = inject(DataService).value;
 }
